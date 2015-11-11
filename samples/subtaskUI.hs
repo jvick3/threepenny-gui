a58e50c42f4e11ace7d1da746304f943961eacf6
James Vickers <jamesvickers19@gmail.com>
2015-11-10 19:56:59 -0700

    working on UI...clicking a Task sets the Subtask list, but appends listBox's to the UI repeatedly (see bottom of setup method...)

--- samples/subtaskUI.hs
+++ samples/subtaskUI.hs
@@ -1,8 +1,16 @@
-
+{-# LANGUAGE RecursiveDo #-}
+module SubtaskUI where
 
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
+import Reactive.Threepenny
+       
 import Control.Monad
+import System.Time
+import Data.Maybe
+import Safe (readMay)
+
+import Subtask
 
 
 main :: IO ()
@@ -11,12 +19,59 @@ main = startGUI defaultConfig setupUI
 
 
 setupUI :: Window -> UI ()
-setupUI window = void $ do
+setupUI window = void $ mdo
         return window # set title "Subtask"
 
-        button <- UI.button #. "button" #+ [string "some button"]
+        -- UI Elements.
 
-        on UI.click button $ (\_ -> do element button # set text "I've been clicked")
-      
-        getBody window #+ [UI.div #. "wrap" #+ [element button]]
+        -- Load tasks from Database, or provide empty lists if none present (using type casts).
+        let t = dummyTask
+        let tasks = [ t ] 
+        let subs = [] :: [Subtask]
+        
+        task_listBox <- UI.listBox (pure tasks) taskSelected (pure $ UI.string . show)
+--        subtask_listBox <- UI.listBox (pure subs) subtaskSelected (pure $ UI.string . show)                
 
+        display <- UI.textarea
+
+        element task_listBox # set (attr "size") "10"
+        
+        getBody window #+ [element task_listBox]
+
+        ---------------------------------------------------------------------------
+        -- Actions/Listeners
+        let eTaskSelect = rumors (UI.userSelection task_listBox)
+            --eSubtaskSelect = rumors (UI.userSelection subtask_listBox)
+
+
+        taskSelected <- stepper Nothing $ eTaskSelect
+
+  
+
+        -- Listener for when a Task is selected.
+        on (rumors . UI.userSelection) task_listBox $
+              (\t -> mdo { 
+                          subtask_listBox <- UI.listBox (pure $ fromJust $ subtasks $ fromJust t) subtaskSelected (pure $ UI.string . show) ;
+                          subtaskSelected <- stepper Nothing $ rumors (UI.userSelection subtask_listBox) ;
+                          element subtask_listBox # set (attr "size") "10" ;
+                          getBody window #+ [element task_listBox, element subtask_listBox, element display]
+                        }
+              ) 
+
+
+        ----------------------------------------------------------
+
+        
+taskWidget :: Task -> UI Element
+taskWidget t = do
+           button <- UI.button #. "button" #+ [string $ tName t]
+           return button
+
+
+-- Functions to make testing versions of Task's and associated Subtask's.           
+dummyTask :: Task
+dummyTask = Task "Dummy task" (TOD 123456789 0) False (Just dummySubTasks)
+
+dummySubTasks :: [Subtask]
+dummySubTasks = [ Subtask "Dummy st 1" False, Subtask "Dummy st 2" True, Subtask "Dummy st 3" True,
+                  Subtask "Dummy st 4" True, Subtask "Dummy st 5" False]
