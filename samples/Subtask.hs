4ec5053b0d3073718a5a3a939ea8d2c91653017d
James Vickers <jamesvickers19@gmail.com>
2015-11-17 13:32:53 -0700

    removed use of FRP (UI.listBox) and redid the task and subtask lists using plain UI.select elements... fully functional prototype complete, including add/delete of tasks and subtasks and marking of subtasks as complete.

--- samples/Subtask.hs
+++ samples/Subtask.hs
@@ -21,7 +21,6 @@ import GHC.Generics
 
 data Task = Task { tName :: String,
                    due :: UTCTime,
-                   taskDone :: Bool,
                    subtasks :: [Subtask] }
                  | NilTask
                  deriving (Generic)
@@ -40,22 +39,32 @@ isNilTask _ = False
 isNilSubtask :: Subtask -> Bool
 isNilSubtask NilSubtask = True
 isNilSubtask _ = False
-     
 
+completeSubtask :: Task -> Subtask -> Task
+completeSubtask (Task name due subs) st = Task name due modSubs
+                                       where modSubs = findAndMark st subs
+
+findAndMark :: Subtask -> [Subtask] -> [Subtask]
+findAndMark _ [] = []            
+findAndMark st (x:xs) = if x == st then (markAsComplete x) : xs
+                                   else (x : (findAndMark st xs) )
+                
+markAsComplete :: Subtask -> Subtask
+markAsComplete (Subtask name done) = Subtask name True
+        
 -- Takes a Task and computes the percentage of it's Subtasks that are done,
 -- i.e. have the property (complete = True)
 percentComplete :: Task -> Float
-percentComplete (Task _ _ True _) = 100.0
-percentComplete (Task _ _ _ subtasks) = if n == 0 then 100.0 else
+percentComplete (Task _ _ subtasks) = if n == 0 then 100.0 else
                                       (sum (map (\st -> if stDone st then 1 else 0) subtasks) / n) * 100.0
                                       where n = fromIntegral $ length subtasks
 
 -- Functions to make testing versions of Task's and associated Subtask's.
 dummyTask :: String -> Task
-dummyTask name = Task name (UTCTime (ModifiedJulianDay 123456789) 0) False (dummySubTasks name)            
+dummyTask name = Task name (UTCTime (ModifiedJulianDay 123456789) 0) (dummySubTasks name)            
 
 dummySubTasks :: String -> [Subtask]
-dummySubTasks name = [ Subtask (name ++ " st " ++ (show i))  False
+dummySubTasks name  = [ Subtask (name ++ " st " ++ (show i)) False
                        | i <- [1..5]  ]
                 
                 
@@ -66,10 +75,9 @@ instance Show Subtask where
 
          
 instance Show Task where
-         show t@(Task name due done st) = "'" ++ name ++ "'; status: " ++ (show status) ++ "; "
+         show t@(Task name due st) = "'" ++ name ++ "';"
                  ++ (show $ length st) ++ " subtasks, "
                  ++ (printf "%.1f" $ percentComplete t) ++ "% complete; due: " ++ (show due)
-                 where status = if done then "complete" else "incomplete"         
          
 
 instance Eq Subtask where
@@ -77,18 +85,18 @@ instance Eq Subtask where
                   (name1 == name2) && (done1 == done2)
          
 instance Eq Task where
-         (Task n1 due1 done1 st1) == (Task n2 due2 done2 st2) =
-               (n1 == n2) && (due1 == due2) && (done1 == done2) && (st1 == st2)
+         (Task n1 due1 st1) == (Task n2 due2 st2) =
+               (n1 == n2) && (due1 == due2) && (st1 == st2)
 
 
 -- Below: compare due dates, with sooner ones being smaller.
 -- Useful for soring a list of Tasks by due date, with soonest ones appearing first.
 -- When due dates are equal, things with lower percentComplete come first (have lower precedence).         
 instance Ord Task where
-         t1@(Task _ due1 _ _) `compare` t2@(Task _ due2 _ _) =
+         t1@(Task _ due1 _) `compare` t2@(Task _ due2 _) =
                (due1 `compare` due2)
                      `mappend`
-               ( (percentComplete t1) `compare` (percentComplete t2) )
+               ( (percentComplete t2) `compare` (percentComplete t1) )
 
 -- Compare by name only for Subtasks
 -- (they might be a set of numbers like ["#1", "#2",...]         
--- samples/subtaskUI.hs
+++ samples/subtaskUI.hs
@@ -3,16 +3,16 @@
 module SubtaskUI where
 
 import qualified Graphics.UI.Threepenny as UI
-import Graphics.UI.Threepenny.Core
+import Graphics.UI.Threepenny.Core hiding (delete)
 import Reactive.Threepenny
        
 import Control.Monad
 import Control.Monad.IO.Class       
---import Control.Monad.Trans
 import Data.Time.Clock
 import Data.Time.Calendar      
 import Data.Maybe
-import Data.List      
+import Data.List
+import Data.IORef       
 
 import Subtask
 
@@ -23,9 +23,8 @@ main = do
      savedTasks <- thaw ".subtasks"
      let tasks = (maybe [] id) savedTasks
          ui_elem = uiLayout tasks in do
-     putStrLn ("tasks = " ++ (show tasks))
+     putStrLn ("Loaded tasks: " ++ (show tasks))
      startGUI defaultConfig (setupUI ui_elem)
-     --TODO: how to recover the modified tasks to persist to file...?
 
 
 
@@ -37,132 +36,190 @@ setupUI ui w = do
 
         
 uiLayout :: [Task] -> UI Element
-uiLayout tasks = mdo
+uiLayout savedTasks = mdo
 
-        -- If taskSelected is 'Just", apply subtasks to it, otherwise the value is just [].
-        let 
-            shown_subtasks :: Behavior [Subtask]
-            shown_subtasks = (maybe [] subtasks) <$> taskSelected
-        
-            -- TODO: because this selectBehavior is Nothing, using arrow keys to select Task
-            -- rather than mouse does not update the subtask list.
-            -- One possible workaround would be to disable arrow keys.
-            selectBehavior = pure Nothing
-        
-
+        -- Make global persistent variable for the [Task] as it is updated.
+        tasks <- liftIO $ newIORef savedTasks
+        taskSelIndex <- liftIO $ newIORef (-1 :: Int)
+         
         -- Create the UI Elements.
-        task_listBox <- UI.listBox b_taskItems selectBehavior (pure $ UI.string . show)
-        subtask_listBox <- UI.listBox shown_subtasks selectBehavior (pure $ UI.string . show)
+        taskList <- UI.select
+        element taskList # set items (toElements savedTasks)
+
+        subtaskList <- UI.select
 
         button_addTask <- UI.button #+ [ string "Add Task" ]
-        button_deleteTask <- UI.button #+ [ string "Delete Task" ]
+        button_addSubtask <- UI.button #+ [ string "Add Subtask" ]
+        button_delete <- UI.button #+ [ string "Delete Task/Subtask" ]
+        button_complete <- UI.button #+ [ string "Mark as complete" ]
 
-        taskNameInput <- UI.input
-        taskMonthInput <- UI.select #+ monthOptions
-        taskDayInput <- UI.select #+ dayOptions
-        taskYearInput <- UI.select #+ yearOptions
+        nameInput <- UI.input
+        monthInput <- UI.select #+ monthOptions
+        dayInput <- UI.select #+ dayOptions
+        yearInput <- UI.select #+ yearOptions
 
         infoBox <- UI.textarea
         --------------------------------------------------------------------------
 
-        -- Actions/Listeners
-        let
-            eTaskSelect :: Event (Maybe Task)  -- Task selected from task_listBox
-            eTaskSelect = rumors $ UI.userSelection task_listBox
-
-            eDeleteTask :: Event ()   -- 'delete task' button clicked
-            eDeleteTask = UI.click button_deleteTask
-
-            eAddTask :: Event ()   -- 'add task' button clicked
-            eAddTask = UI.click button_addTask        
-
-        -- taskSelected :: Behavior (Maybe Task)        
-        taskSelected <- stepper Nothing $ head <$> unions
-               [
-                eTaskSelect,
-                -- Change selection to Nothing if delete is pressed 
-                Nothing <$ eDeleteTask
-               ]
-
+        on UI.click button_addSubtask $ \_ -> do
+                name <- get value nameInput
+                if name == "" then do
+                   element infoBox # set UI.text "Error: enter a subtask name"
+                else do
+                   currentTasks <- liftIO $ readIORef tasks
+                   taskIndex <- get UI.selection taskList
+                   if isNothing taskIndex then do
+                      element infoBox # set UI.text "Error: select a Task to add a new subtask for"
+                   else do
         
-        let addTask :: [Task] -> [Task]
-            addTask existingTasks = do
-                    let t = dummyTask "Fuck"
-                    t : existingTasks
---                
---                liftIO $ putStrLn "foo"
---                [dummyTask "bs 1", dummyTask "bs 2"]
-                --name <- get value taskNameInput        
-                --if name == "" then do
-                --   existingTasks  -- just return the tasks as-is
-                --else do
-                --     year <- get value taskYearInput        
-                --     month <- get value taskMonthInput
-                --     day <- get value taskDayInput
+                        let selectedTask = currentTasks !! (fromJust taskIndex)
+                            newSubtask = Subtask name False
+                            updatedSubtasks = newSubtask : (subtasks selectedTask)
+                            updatedTask = Task (tName selectedTask) (due selectedTask) updatedSubtasks
+                            updatedTasks = updatedTask : (deleteNth (fromJust taskIndex) currentTasks)
 
-                --     existingTasks
-                     --let deadline = convertToTime year month day
-                     --    newTask = Task name deadline False []
+                        -- Persist tasks and update the global tasks variable.
+                        liftIO $ persist updatedTasks ".subtasks"
+                        liftIO $ writeIORef tasks updatedTasks
+
+                        -- Update displays.
+                        element taskList # set items (toElements updatedTasks)
+                        element subtaskList # set items (toElements updatedSubtasks)        
+                        element infoBox # set UI.text ("Created subtask: " ++ name)
+                        element nameInput # set value ""  -- clear name input field
+        ---------------------------------------------------------------------------------                        
         
-                     --(newTask : existingTasks)
-        
-
---        b_taskItems :: Behavior [Task]
-        b_taskItems <- accumB tasks $ concatenate <$> unions 
-              [ addTask <$ eAddTask
-                -- deleteTask tasks <$ eDeleteTask
-              ]
-
-        -- Add/Delete button behaviors:
-        -- 1. Populate info box with success state or error message,
-        -- 2. persist the changes to the '.subtasks' file.
+        -- Add button behavior: check if 'name' input has a value, and if so add a Task to the list
+        -- using that name and the values for the year/month/day inputs.
         on UI.click button_addTask $ \_ -> do
-                name <- get value taskNameInput
+
+                name <- get value nameInput
                 if name == "" then do
                    element infoBox # set UI.text "Error: enter a task name"
                 else do
-                     existingTasks <- currentValue b_taskItems
-                     liftIO $ putStrLn ("existing tasks = " ++ (show existingTasks))
-                     liftIO $ persist existingTasks ".subtasks"
-                     element infoBox # set UI.text "Created task"
+                     year <- get value yearInput
+                     month <- get value monthInput
+                     day <- get value dayInput
+                     currentTasks <- liftIO $ readIORef tasks                    
+                     let newTask =  Task name (convertToTime year month day) []
+                         updatedTasks = newTask : currentTasks
+                     -- Persist tasks and update the global tasks variable.
+                     liftIO $ persist updatedTasks ".subtasks"
+                     liftIO $ writeIORef tasks updatedTasks
 
-        
-        on UI.click button_deleteTask $ \_ -> do
-                element infoBox # set UI.text "Deleted task"
-        
+                     -- Update displays.
+                     element taskList # set items (toElements updatedTasks)
+                     element infoBox # set UI.text ("Created task: " ++ name)
+                     element nameInput # set value ""  -- clear name input field        
+        --------------------------------------------------------------------------
 
+        on UI.click button_complete $ \_ -> do
+                subtaskIndex <- get UI.selection subtaskList
+                if isNothing subtaskIndex then do
+                     element infoBox # set UI.text "Error: select a Subtask to mark as complete"
+                else do
+                     currentTasks <- liftIO $ readIORef tasks                     
+                     tIndex <- liftIO $ readIORef taskSelIndex
+                     if tIndex < 0 then do
+                        element infoBox # set UI.text "Error: select a Subtask to mark as complete"                         
+                     else do                     
+                           let selectedTask = currentTasks !! tIndex
+                               selectedSubtask = (subtasks selectedTask) !! (fromJust subtaskIndex)
+                               updatedTask = completeSubtask selectedTask selectedSubtask
+                               updatedTasks = updatedTask : (deleteNth tIndex currentTasks)        
+        
+                           liftIO $ persist updatedTasks ".subtasks"
+                           liftIO $ writeIORef tasks updatedTasks
+
+                           -- Update displays.
+                           element taskList # set items (toElements updatedTasks)
+                           element subtaskList # set items (toElements $ subtasks updatedTask)        
+                           element infoBox # set UI.text ("Marked subtask " ++ (stName selectedSubtask) ++ " complete")
+        
+        ---------------------------------------------------------------------------------                
+        on UI.click button_delete $ \_ -> do
+                taskIndex <- get UI.selection taskList
+                if isNothing taskIndex then do
+                   subtaskIndex <- get UI.selection subtaskList
+                   if isNothing subtaskIndex then do
+                      element infoBox # set UI.text "Error: select Task or Subtask to delete"
+                   else do
+                      currentTasks <- liftIO $ readIORef tasks
+                      tIndex <- liftIO $ readIORef taskSelIndex
+                      if tIndex < 0 then do
+                         element infoBox # set UI.text "Error: select Task or Subtask to delete"
+                      else do
+                           let selectedTask = currentTasks !! tIndex
+                               currentSubtasks = subtasks selectedTask
+                               updatedSubtasks = deleteNth (fromJust subtaskIndex) currentSubtasks
+                               updatedTask = Task (tName selectedTask) (due selectedTask) updatedSubtasks
+                               updatedTasks = updatedTask : (deleteNth tIndex currentTasks)
+                           liftIO $ persist updatedTasks ".subtasks"
+                           liftIO $ writeIORef tasks updatedTasks
+
+                           -- Update displays.
+                           element taskList # set items (toElements updatedTasks)
+                           element subtaskList # set items (toElements updatedSubtasks)        
+                           element infoBox # set UI.text "Deleted subtask "
+                               
+                else do
+                     currentTasks <- liftIO $ readIORef tasks
+                     let updatedTasks = deleteNth (fromJust taskIndex) currentTasks
+
+                     -- Persist the tasks list both in the global 'tasks' variable and local file system.
+                     liftIO $ persist updatedTasks ".subtasks"
+                     liftIO $ writeIORef tasks updatedTasks
+
+                     -- Update the UI components as appropriate.
+                     element taskList # set items (toElements updatedTasks)        
+                     element infoBox # set UI.text "Deleted task"
+        ---------------------------------------------------------------------------------                
+                
+        -- When Task is selected, populate the subtaskList with the subtasks of that Task.
+        on UI.selectionChange taskList $ \e -> do
+                currentTasks <- liftIO $ readIORef tasks
+                let index = fromJust e
+                    chosenTask = currentTasks !! index
+        
+                -- track task index; useful when subtasks are displayed but their parent task is no longer selected.
+                liftIO $ writeIORef taskSelIndex index 
+                element subtaskList # set items (toElements (subtasks chosenTask))
+        ---------------------------------------------------------------------------------
+        
         -- Set element styles and sizes
-        element task_listBox # set (attr "size") "10"
-        element subtask_listBox # set (attr "size") "10"
+        element taskList # set (attr "size") "10"
+        element subtaskList # set (attr "size") "10"
         element infoBox # set style [("width","500px"),("height","40px"),("border","solid black 1px")]
 
         ----------------------------------------------------------        
 
         -- Return a grid layout of the UI elements
         grid [ 
-                   [element task_listBox, element subtask_listBox],
+                   [element taskList, element subtaskList],
 
-                   [element button_addTask, element button_deleteTask],
+                   [element button_addTask, element button_addSubtask, element button_delete, element button_complete],
 
-                   [string "New task name:", element taskNameInput],
+                   [string "Task/Subtask name:", element nameInput],
 
-                   [string "Deadline month: ", element taskMonthInput],
+                   [string "Task Deadline month: ", element monthInput],
 
-                   [string "Deadline day: ", element taskDayInput],
+                   [string "Task Deadline day: ", element dayInput],
 
-                   [string "Deadline year: ", element taskYearInput],
+                   [string "Task Deadline year: ", element yearInput],
 
                    [string "Info: ", element infoBox]
                
              ]
         ---------------------------------------------------------------------------
 
+deleteNth :: Int -> [a] -> [a]
+deleteNth _ []     = []
+deleteNth n (x:xs)
+           | n == 0    = xs
+           | otherwise = x : deleteNth (n-1) xs
         
-tryCreateTask :: UI Element -> UI Element -> UI Element -> UI Element -> Maybe Task
-tryCreateTask nameInp yearInp monthInp dayInp = do
-              name <- get value nameInp
-              Just $ dummyTask "fuck"
-        
+toElements :: (Show a) =>  [a] -> [UI Element]
+toElements = map (UI.string . show)
         
 convertToTime :: String -> String -> String -> UTCTime
 convertToTime year month day = UTCTime ( fromGregorian y m d ) (86400 / 2)
@@ -186,10 +243,7 @@ monthNames :: [String]
 monthNames = [ "January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"]
         
-redoLayout :: Window -> UI ()
-redoLayout w = void $ do
-           getBody w # set children []
-        
-
+items = mkWriteAttr $ \i x -> void $ do
+        return x # set children [] #+ map (\i -> UI.option #+ [i]) i
 
 
